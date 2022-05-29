aca32084416faa503c83ce578b8281d0de1a2283
Martin <persky03@gmail.com>
2022-05-29 21:17:58 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+console.log("test!");
+let age = 29;
+const iterations = 22;
+const add = (v1, v2) => v1 + v2;
+console.log(add(10, 20));
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+console.log("test!");
+
+let age: number = 29;
+
+const iterations: number = 22;
+
+const add = (v1: number, v2: number) => v1 + v2;
+
+console.log(add(10, 20))
